b61bce258301afb16cda222ab577a96e4f4577cf
Mustafa Eideh <93211764+Musta-eideh@users.noreply.github.com>
2025-09-16 09:57:53 -0300

    first commit

---
+++ src/main/java/com/stockmachine/stock/machine/StockMachineApplication.java
@@ -0,0 +1,13 @@
+package com.stockmachine.stock.machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockmachine/stock/machine/StockMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockmachine.stock.machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
